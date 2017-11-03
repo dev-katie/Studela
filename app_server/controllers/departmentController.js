f1d595991b0a6fa5ca993c8c244be06d9a1a5f36
dev-katie <write2cathy@rocketmail.com>
2017-11-03 16:39:15 +0100

    modified the path of the api

--- app_server/controllers/departmentController.js
+++ app_server/controllers/departmentController.js
@@ -2,9 +2,10 @@ var request = require('request');
 var apiOptions = {
     server: "http://localhost:3000"
 };
-/* if (process.env.NODE_ENV === 'production'){
-    apiOptions.server = "https://loca8r.herokuapp.com";
-} */
+if (process.env.NODE_ENV === 'production'){
+    apiOptions.server = "https://studela.herokuapp.com";
+} 
+
 
 //catching errors
 var _showError = function (req, res, status){
